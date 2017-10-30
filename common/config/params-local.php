27fee65c4f7eeb4bc4569d43af7d8771beccc780
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-10-30 15:20:45 +0800

    initial commit

---
+++ common/config/params-local.php
@@ -0,0 +1,14 @@
+<?php
+yii::setAlias('@path1', '/api/ci-rest-api-techelm-test');
+//yii::setAlias('@api-image', '/api/techelm-client/uploads/capture');
+yii::setAlias('@api-image', '/api/v2.4/techelm-crm/uploads/capture');
+yii::setAlias('@api-audio', '/api/v2.4/techelm-crm/uploads/recording');
+yii::setAlias('@api-signature', '/api/v2.4/techelm-crm');
+yii::setAlias('@drawing', '/api/v2.2/techelm-client/uploads/drawing');
+yii::setAlias('@api-main', '/api/v2.4/techelm-crm');
+//yii::setAlias('@api-image-old', '/api/techelm-client/uploads/capture');
+//yii::setAlias('@api-audio-old', '/api/techelm-client/uploads/recording');
+
+return [
+
+];
