5e9ee240e99470fc9070cfbf1d7ec8b59397521f
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-06 17:26:20 +0800

    whitespace removal

--- common/models/SearchServicejob.php
+++ common/models/SearchServicejob.php
@@ -190,8 +190,6 @@ class SearchServicejob extends Servicejob
 
     //  $this->load($params);
 
-
-
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
