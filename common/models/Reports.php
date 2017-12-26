17a0bb5bfc854c0804f25035171e7b44f7cfab3a
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-26 17:11:31 +0800

    added sort on all reports

--- common/models/Reports.php
+++ common/models/Reports.php
@@ -51,13 +51,13 @@ class Reports extends Servicejob
     $dataProvider = new ActiveDataProvider([
         'query' => $query,
         'pagination'=>false,
-    //    'pagination'=>[
-    //      'pageSize'=>1,
-    //    ],
+        'pagination'=>[
+          'pageSize'=>10,
+        ],
         'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
     ]);
-    //$dataProvider->query->where(['active' => 1
-    $dataProvider->query->all();
+
+    //$dataProvider->query->all();
 
     $this->load($params);
 
@@ -71,31 +71,34 @@ class Reports extends Servicejob
         return $dataProvider;
     }
 
-    // grid filtering conditions
-    $query->andFilterWhere([
-        'id' => $this->id,
-        'customer_id' => $this->customer_id,
-        'service_id' => $this->service_id,
-        'engineer_id' => $this->engineer_id,
-        'start_date' => $this->start_date,
-        'end_date' => $this->end_date,
-        'status' => $this->status,
-        'start_date_task' => $this->start_date_task,
-        'end_date_task' => $this->end_date_task,
-        'active'=>$this->active,
-        'service_no'=>$this->service_no
-    ]);
+      // grid filtering conditions
+    if (!empty($this->service_no) || !empty($this->customer_id) || !empty($this->service_id) || !empty($this->engineer_id) || !empty($this->status) || !empty($this->active)  || !empty($this->service_date) || !empty($this->remarks) || !empty($this->year) ) {
+      $query->andFilterWhere([
+          'id' => $this->id,
+          'customer_id' => $this->customer_id,
+          'service_id' => $this->service_id,
+          'engineer_id' => $this->engineer_id,
+          'start_date' => $this->start_date,
+          'end_date' => $this->end_date,
+          'status' => $this->status,
+          'start_date_task' => $this->start_date_task,
+          'end_date_task' => $this->end_date_task,
+          'active'=>$this->active,
+          'service_no'=>$this->service_no
+      ]);
 
-  //  $query->andFilterWhere(['like', 'service_no', $this->service_no])
-      $query->andFilterWhere(['like', 'remarks', $this->remarks])
-        ->andFilterWhere(['like', 'remarks_before', $this->remarks_before])
-        ->andFilterWhere(['like', 'remarks_after', $this->remarks_after])
-        ->andFilterWhere(['like', 'equipment_type', $this->equipment_type])
-        ->andFilterWhere(['like', 'serial_no', $this->serial_no])
-        ->andFilterWhere(['like', 'signature_name', $this->signature_name])
-        ->andFilterWhere(['between', 'service_Date', $start,$end])
-        ->andFilterWhere(['like', 'service_Date', $this->year]);
-
+    //  $query->andFilterWhere(['like', 'service_no', $this->service_no])
+        $query->andFilterWhere(['like', 'remarks', $this->remarks])
+          ->andFilterWhere(['like', 'remarks_before', $this->remarks_before])
+          ->andFilterWhere(['like', 'remarks_after', $this->remarks_after])
+          ->andFilterWhere(['like', 'equipment_type', $this->equipment_type])
+          ->andFilterWhere(['like', 'serial_no', $this->serial_no])
+          ->andFilterWhere(['like', 'signature_name', $this->signature_name])
+          ->andFilterWhere(['between', 'service_Date', $start,$end])
+          ->andFilterWhere(['like', 'service_Date', $this->year]);
+    }else{
+      $query->andFilterWhere(['id'=>0]);
+    }
 
     return $dataProvider;
   }
@@ -103,31 +106,56 @@ class Reports extends Servicejob
   public function report_d($params){
 
       $query =ServicejobParts::find()
-          ->select(['servicejob.service_no','servicejob.customer_id','servicejob.engineer_id','servicejob_parts.parts_name','servicejob_parts.quantity',
+          ->select(['servicejob.service_no','servicejob.customer_id','servicejob.engineer_id','servicejob.service_date','servicejob_parts.parts_name','servicejob_parts.quantity',
             'servicejob_parts.unit_price','servicejob_parts.total_price','servicejob_parts.servicejob_id'])
-          ->JOIN('LEFT JOIN', 'servicejob','servicejob.id=servicejob_parts.servicejob_id')
+          ->JOIN('LEFT JOIN', 'servicejob','servicejob.id=servicejob_parts.servicejob_id');
           //->with('servicejob');
-          ->orderBy(['servicejob_parts.id'=>SORT_DESC]);
-        //  ->groupBy('servicejob_parts.servicejob_id');
+        //  ->orderBy(['servicejob_parts.id'=>SORT_DESC]);
+      //    ->groupBy('servicejob_parts.servicejob_id');
 
       $dataProvider = new ActiveDataProvider([
           'query' => $query,
-          'pagination'=>false,
-        //  'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
+         'pagination'=>false,
+          'sort'=> ['defaultOrder' => ['servicejob_id'=>SORT_DESC]]
       ]);
-    
-      $dataProvider->query->all();
+
+    /*  $dataProvider->sort->attributes['service_no'] = [
+        'asc' => ['servicejob.service_no' => SORT_ASC],
+        'desc' => ['servicejob.service_no' => SORT_DESC],
+    ];*/
+
+    $dataProvider->setSort([
+      'attributes'=>[
+        'parts_name',
+        'service_no',
+        'service_date',
+        'quantity',
+        'unit_price',
+        'total_price',
+      ],
+    ]);
+
+      //$dataProvider->query->all();
 
       $this->load($params);
 
-      $query->andFilterWhere([
-          'customer_id' => $this->customer_id,
-          'engineer_id' => $this->engineer_id,
-            //    'service_id' => $this->service_id,
-      ]);
+      if (!empty($this->service_date)) {
+        list($this->start,$this->end)= explode(' - ',$this->service_date);
+      }
 
-      $query->andFilterWhere(['like','servicejob.service_no',$this->service_no])
-            ->andFilterWhere(['like','servicejob_parts.parts_name',$this->parts]);
+      if (!empty($this->service_no) || !empty($this->parts) || !empty($this->customer_id) || !empty($this->engineer_id) || !empty($this->service_date) ) {
+        $query->andFilterWhere([
+            'customer_id' => $this->customer_id,
+            'engineer_id' => $this->engineer_id,
+              //    'service_id' => $this->service_id,
+        ]);
+
+        $query->andFilterWhere(['like','servicejob.service_no',$this->service_no])
+              ->andFilterWhere(['like','servicejob_parts.parts_name',$this->parts])
+              ->andFilterWhere(['between', 'servicejob.service_Date', $this->start,$this->end]);
+      }else {
+        $query->andFilterWhere(['servicejob_parts.id'=>0]);
+      }
 
       return $dataProvider;
   }
@@ -167,6 +195,76 @@ class Reports extends Servicejob
 
   //  $rows->andFilterWhere(['like', 'service_no', $this->service_no]);
     return $result;
+  }
+
+  public function report_ba($params){
+  //  $query =Servicejob::find()->limit(10);
+    $query = new Query();
+
+        $query->select(['servicejob.id as ids', 'servicejob.service_no','servicejob.customer_id','servicejob.engineer_id','servicejob.service_date','servicejob.status','servicejob.active','servicejob.remarks',
+            'servicejob_complaint_mobile.servicejob_id','servicejob_complaint_mobile.id',  'servicejob_complaint_mobile.servicejob_category_id',  'servicejob_complaint_mobile.complaint_name',  'servicejob_complaint_mobile.complaint_remark',
+            'servicejob_cm_asr.servicejob_cm_cf_id','servicejob_cm_asr.servicejob_action_service_repair_id','servicejob_cm_cf.id','customer.fullname','user.fullname as eng_name', 'servicejob_categories.category as com_cat', 'servicejob_action_service_repair.action'
+        ])
+        ->from('servicejob')
+        ->join('LEFT JOIN', 'servicejob_complaint_mobile', 'servicejob.id = servicejob_complaint_mobile.servicejob_id')
+        ->join('LEFT JOIN', 'servicejob_cm_cf', 'servicejob_complaint_mobile.id = servicejob_cm_cf.servicejob_complaint_mobile_id')
+        ->join('LEFT JOIN', 'servicejob_cm_asr', 'servicejob_cm_asr.servicejob_cm_cf_id=servicejob_cm_cf.id')
+        ->join('LEFT JOIN', 'customer', 'customer.id=servicejob.customer_id')
+        ->join('LEFT JOIN', 'user', 'user.id=servicejob.engineer_id')
+        ->join('LEFT JOIN', 'servicejob_categories', 'servicejob_categories.id=servicejob_complaint_mobile.servicejob_category_id')
+        ->join('LEFT JOIN', 'servicejob_action_service_repair', 'servicejob_action_service_repair.id=servicejob_cm_asr.servicejob_action_service_repair_id');
+        //->limit(10);
+    //  $query->groupBy(['servicejob.service_no']);
+
+      $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            //'pagination'=>false,
+          //  'sort'=> ['defaultOrder' => ['ids'=>SORT_DESC]]
+      ]);
+
+      $dataProvider->setSort([
+        'attributes'=>[
+          'service_no',
+          'eng_name',
+          'fullname',
+          'engineer_id',
+          'service_date',
+          'status',
+          'com_cat',
+          'complaint_name',
+          'action',
+        ],
+      ]);
+
+      $this->load($params);
+
+      if (!empty($this->service_date)) {
+        list($this->start,$this->end)= explode(' - ',$this->service_date);
+      }
+
+      if (!empty($this->service_no) || !empty($this->customer_id) || !empty($this->engineer_id) || !empty($this->status) || !empty($this->service_date) || !empty($this->year) || !empty($this->complaint_cat) || !empty($this->complaint) || !empty($this->action) ||
+        !empty($this->remarks)
+     ) {
+
+              $query->andFilterWhere(['customer_id'=>$this->customer_id,
+                    'engineer_id' => $this->engineer_id,
+                    'servicejob.status'=>$this->status,
+                  //  'servicejob.active'=>$this->active,
+                    'servicejob_complaint_mobile.servicejob_category_id'=>$this->complaint_cat,
+                    'servicejob_action_service_repair_id'=>$this->action,
+                    'servicejob.active'=>1,
+                  ])
+              ->andFilterWhere(['like', 'service_no', $this->service_no])
+              ->andFilterWhere(['between', 'service_Date', $this->start,$this->end])
+              ->andFilterWhere(['like', 'service_Date', $this->year])
+              ->andFilterWhere(['like','complaint_name',$this->complaint])
+              ->andFilterWhere(['like','remarks',$this->remarks]);
+      }else{
+          $query->andFilterWhere(['servicejob.id'=>0]);
+      }
+
+
+      return $dataProvider;
   }
 
 
