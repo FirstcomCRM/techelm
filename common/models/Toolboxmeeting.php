eccf6713ee2e8bcbeccba5916e07284105ac013d
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-26 17:12:33 +0800

    added new validations

--- common/models/Toolboxmeeting.php
+++ common/models/Toolboxmeeting.php
@@ -17,6 +17,7 @@ use Yii;
  * @property string $signature
  * @property integer $status_flag
  * @property string $date_added
+ * @property string $site_address
  */
 class Toolboxmeeting extends \yii\db\ActiveRecord
 {
@@ -34,11 +35,12 @@ class Toolboxmeeting extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['projectjob_id', 'meeting_image', 'image_uploaded_date', 'meeting_details', 'conducted_by', 'designation', 'signature', 'status_flag_tm', 'date_added'], 'required'],
+            [['site_address', 'conducted_by'], 'required'],
             [['projectjob_id', 'status_flag_tm'], 'integer'],
             [['image_uploaded_date', 'date_added'], 'safe'],
-            [['meeting_image', 'conducted_by', 'designation', 'signature'], 'string', 'max' => 255],
+            [['meeting_image', 'conducted_by', 'signature'], 'string', 'max' => 255],
             [['meeting_details'], 'string', 'max' => 500],
+            [['site_address'],'string'],
         ];
     }
 
@@ -54,10 +56,10 @@ class Toolboxmeeting extends \yii\db\ActiveRecord
             'image_uploaded_date' => 'Image Uploaded Date',
             'meeting_details' => 'Meeting Details',
             'conducted_by' => 'Conducted By',
-            'designation' => 'Designation',
             'signature' => 'Signature',
             'status_flag_tm' => 'Status Flag',
             'date_added' => 'Date Added',
+            'site_address'=>'Site Address'
         ];
     }
 }
