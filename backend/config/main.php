962409c158b1a0f7bce769f51f9404efc34160dc
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-26 16:56:31 +0800

    initial commit

--- backend/config/main.php
+++ backend/config/main.php
@@ -42,7 +42,8 @@ return [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    //'levels' => ['error', 'warning'],
+                    'levels' => ['info', 'warning'],
                 ],
             ],
         ],
